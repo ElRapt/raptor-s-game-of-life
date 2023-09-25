e298884e54d0a54ea67c9c41cbf31c2b3bd56226
Lucas Denis <lucas.denis@etu.uca.fr>
2023-09-25 20:05:39 +0200

    Name changes and live and die method definition

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <SFML/Graphics.hpp>
 
+void kill(sf::Uint8* grid, int x, int y, int width)
+{
+	grid[(y * width + x) * 4 + 0] = 0;
+	grid[(y * width + x) * 4 + 1] = 0;
+	grid[(y * width + x) * 4 + 2] = 0;
+	grid[(y * width + x) * 4 + 3] = 255;
+
+}
+
+void create(sf::Uint8* grid, int x, int y, int width)
+{
+    grid[(y * width + x) * 4 + 0] = 255;
+    grid[(y * width + x) * 4 + 1] = 255;
+    grid[(y * width + x) * 4 + 2] = 255;
+    grid[(y * width + x) * 4 + 3] = 255;
+
+}
+
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    const int width = 1920;
+    const int height = 1080;
+    sf::Uint8* grid = new sf::Uint8[width * height * 4];
+
+
+
+
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "RaptorsGameOfLife" };
     window.setFramerateLimit(144);
 
     while (window.isOpen())
