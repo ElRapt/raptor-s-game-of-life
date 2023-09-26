0e23c41b1c3dd051d06f9c64205a437c53d175ea
Lucas Denis <lucas.denis@etu.uca.fr>
2023-09-26 20:55:14 +0200

    Rewrote logic into rectangles

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,54 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <random>
+#include <omp.h>
 
-void kill(sf::Uint8* grid, int x, int y, int width)
-{
-	grid[(y * width + x) * 4 + 0] = 0;
-	grid[(y * width + x) * 4 + 1] = 0;
-	grid[(y * width + x) * 4 + 2] = 0;
-	grid[(y * width + x) * 4 + 3] = 255;
+const int gridWidth = 100;
+const int gridHeight = 100;
+const int cellWidth = 19;  // 1920 / 100
+const int cellHeight = 10; // 1080 / 100
 
+
+void create(sf::RectangleShape& rect, sf::RenderWindow& window, int x, int y) {
+    rect.setPosition(x * cellWidth, y * cellHeight);
+    window.draw(rect);
 }
 
-void create(sf::Uint8* grid, int x, int y, int width)
-{
-    grid[(y * width + x) * 4 + 0] = 255;
-    grid[(y * width + x) * 4 + 1] = 255;
-    grid[(y * width + x) * 4 + 2] = 255;
-    grid[(y * width + x) * 4 + 3] = 255;
 
-}
 
 int main()
 {
-    const int width = 1920;
-    const int height = 1080;
-    sf::Uint8* grid = new sf::Uint8[width * height * 4];
+    const int width = 800;
+    const int height = 600;
+
+
+    // Initialize the Mersenne Twister PRNG with a seed
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(1, 10);
 
 
 
+    sf::RectangleShape rect(sf::Vector2f(cellWidth, cellHeight));
+    rect.setFillColor(sf::Color::White);
 
     auto window = sf::RenderWindow{ { 1920u, 1080u }, "RaptorsGameOfLife" };
     window.setFramerateLimit(144);
+
+    #pragma omp parallel for
+    for (int i = 0; i < gridHeight; i++)
+    {
+        for (int j = 0; j < gridWidth; j++)
+        {
+            int random_number = dist(mt);
+            if (random_number == 1) {
+                std::cout << "Added cell at " << j << ", " << i << std::endl;
+                create(rect, window, j, i);
+            }
+        }
+    }
+
+
 
     while (window.isOpen())
     {
@@ -40,7 +60,9 @@ int main()
             }
         }
 
-        window.clear();
         window.display();
     }
+
+    return 0;
+
 }
