32c928fceab3bad0092e1a51986f307dcb734c6a
Lucas Denis <lucas.denis@etu.uca.fr>
2023-10-12 10:13:15 +0200

    Added a pause feature

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,8 +6,6 @@
 const float PI = 3.14159265358979323846;
 
 
-// TODO : Fix the grid rendering
-
 Game::Game(int width, int height) : grid(width, height), window(sf::VideoMode(1920, 1080), "Game of Life") {
     grid.init();
     window.setFramerateLimit(15);
@@ -29,7 +27,9 @@ std::vector<sf::Vector2f> calculateHexagonVertices(float centerX, float centerY,
 }
 
 void Game::run() {
-    // Fixed hexagon radius, as per your requirement
+    bool isPaused = false;  
+    bool needsRedraw = true;  
+
     float hexRadius = 20.0f;
     float dx = hexRadius * sqrt(3);
     float dy = hexRadius * 1.5;
@@ -40,46 +40,59 @@ void Game::run() {
     hexagon.setOrigin(hexRadius, hexRadius);
     hexagon.setOutlineColor(sf::Color::Black);
 
-    // Calculate the offsets to center the grid based on dynamic width and height
     float offsetX = (1920 - dx * grid.width) / 2;
     float offsetY = (1080 - dy * grid.height) / 2;
 
     while (window.isOpen()) {
-        for (auto event = sf::Event{}; window.pollEvent(event);) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-        }
 
-        window.clear();  // Clear to background color
-
-        for (int y = 0; y < grid.height; ++y) {
-            for (int x = 0; x < grid.width; ++x) {
-                int age = grid.getCellAge(x, y);
-                sf::Uint8 red = static_cast<sf::Uint8>(255 * age / maxAge);
-
-                // Calculate position; every other row is offset by half
-                float rowOffset = (y % 2 == 0) ? 0 : (dx / 2);
-                float posX = x * dx + offsetX + rowOffset;
-                float posY = y * dy + offsetY;
-
-                hexagon.setPosition(posX, posY);
-
-                if (grid.getCellState(x, y)) {
-                    hexagon.setFillColor(sf::Color(red, 255 - red, 255 - red));
-                }
-                else {
-                    hexagon.setFillColor(sf::Color::White);
-                }
-
-                window.draw(hexagon);
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P) {
+                isPaused = !isPaused;
+                needsRedraw = true;  // Frame needs to be redrawn when pausing or resuming
             }
         }
 
-        window.display();
-        grid.update();
+        if (!isPaused || needsRedraw) {
+            window.clear();  
+
+            for (int y = 0; y < grid.height; ++y) {
+                for (int x = 0; x < grid.width; ++x) {
+                    int age = grid.getCellAge(x, y);
+                    sf::Uint8 red = static_cast<sf::Uint8>(255 * age / maxAge);
+
+
+                    float rowOffset = (y % 2 == 0) ? 0 : (dx / 2);
+                    float posX = x * dx + offsetX + rowOffset;
+                    float posY = y * dy + offsetY;
+
+                    hexagon.setPosition(posX, posY);
+
+                    if (grid.getCellState(x, y)) {
+                        hexagon.setFillColor(sf::Color(red, 255 - red, 255 - red));
+                    }
+                    else {
+                        hexagon.setFillColor(sf::Color::White);
+                    }
+
+                    window.draw(hexagon);
+                }
+            }
+
+            if (!isPaused) {
+                grid.update();  
+            }
+
+            window.display();  
+            needsRedraw = false; 
+        }
     }
 }
+
+
 
 
 
